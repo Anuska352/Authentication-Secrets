417c868bebe4d0e7a7f61337ac498b46a49768bb
Anuska352 <anuskachattopadhyay561@gmail.com>
2023-06-16 21:01:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+main().catch(err => console.log(err));
+ 
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    const userSchema = new mongoose.Schema({
+        email:String,
+        password:String
+    });
+ 
+const User = mongoose.model('User',userSchema);
+
 app.get("/", function(req,res){
     res.render("home");
 });
@@ -22,10 +34,48 @@ app.get("/register", function(req,res){
     res.render("register");
 });
 
+app.post("/register",async(req,res)=>{
+    try {
+        const newUser = new User({
+            email:req.body.username,
+            password:req.body.password
+        });
+        const result = await newUser.save();
+        if(result){
+            res.render('secrets');
+        }else{
+            console.log("Login Failed");
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
 
+
+app.post("/login",async(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        const foundName = await User.findOne({email:username})
+        if(foundName){
+            if(foundName.password===password){
+                res.render('secrets');
+            }else{
+                console.log('Password Does not Match...Try Again !')
+            }
+        }else{
+            console.log("User Not found...")
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 
 
 app.listen(3000, function(){
     console.log("Server started on port 3000.");
-})
+});
+
+}
